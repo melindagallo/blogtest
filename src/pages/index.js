72b10eb7aa0d919947602fa9fc88c4a04afd592d
melindagallo <68917193+melindagallo@users.noreply.github.com>
2021-04-15 13:58:51 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,6 +30,7 @@ const BlogIndex = ({ data, location }) => {
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
+          const category = post.category.name
 
           return (
             <li key={post.fields.slug}>
@@ -41,7 +42,7 @@ const BlogIndex = ({ data, location }) => {
                 <header>
                   <h2>
                     <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
+                      <span itemProp="headline">{category} :: {title}</span>
                     </Link>
                   </h2>
                   <small>{post.frontmatter.date}</small>
