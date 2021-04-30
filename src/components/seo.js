47308834cb5bd277378abdd8c729cd7d970bed66
melindagallo <68917193+melindagallo@users.noreply.github.com>
2021-04-30 22:45:04 +0200

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -26,6 +26,26 @@ const SEO = ({ description, lang, meta, title }) => {
       }
     `
   )
+const structuredData = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Visby Medical",
+  "url": "https://www.visbymedical.com/",
+  "logo": "https://media.visbymedical.com/VisbyLogoTM.png",
+  "contactPoint": {
+	 "@type": "ContactPoint",
+	 "telephone": "833-468-4729",
+	 "contactType": "customer service",
+	 "contactOption": "TollFree",
+	 "areaServed": "US",
+	 "availableLanguage": "en"
+  },
+  "sameAs": [
+	"https://www.linkedin.com/company/visbymedical",
+	"https://twitter.com/visbymedical",
+	"https://www.facebook.com/visbymedical/"
+  ]
+}
 
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
@@ -70,8 +90,9 @@ const SEO = ({ description, lang, meta, title }) => {
           name: `twitter:description`,
           content: metaDescription,
         },
-      ].concat(meta)}
-    />
+      ].concat(meta)}>
+	  <script type="application/ld+json"> { JSON.stringify(structuredData) } </script>
+</Helmet>
   )
 }
 
